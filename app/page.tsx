4e5ed63ce01c88956d32dff1dfd4e302f1a7b987
sha1kh4 <adnan.shaikh166@mit.asia>
2026-02-09 17:25:53 +0530

    create basic headertabs

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Header  from "@/components/header";
-
+import { HeaderTabs } from "@/components/head-tabs";
 export default function Home() {
   return (
     <>
-      <Header></Header>
+      <Header/>
+      <HeaderTabs/>
     </>
   );
 }
---
+++ components/head-tabs.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarGroup,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { useEffect, useState } from "react";
+
+export function HeaderTabs() {
+  const [categories, setCategories] = useState<string[]>([]);
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+
+        const cats: string[] = [
+          ...new Set(data.map((item: { category: string }) => item.category)),
+        ];
+        setCategories(cats);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+    fetchCategories();
+  }, []);
+  return (<>
+       <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+  
+        {
+            categories.map((category) => (
+                <div className="mr-5 hover:text-gray-900" key={category}>
+                    <h2>{category}</h2>
+                </div>
+            ))
+        }
+    
+      
+    </nav>
+    
+    </>
+  );
+}
